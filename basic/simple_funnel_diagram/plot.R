53a856d0539ee2d7f26f9a7cf12ecaa96864a29d
lxs <liang09255@gmail.com>
2023-07-17 12:33:25 +0800

    feat: add new drawing plugin - simple funnel diagram (#4)
    * feat: add deviation_plot
    
    * fix: set Value and sort_method to required
    
    * fix: change date format
    
    * feat: add new drawing plugin - Circular bar plot
    
    * feat: add new drawing plugin - custom heatmap
    
    * feat: add new drawing plugin - simple funnel diagram

---
+++ basic/simple_funnel_diagram/plot.R
@@ -0,0 +1,21 @@
+library(magrittr)
+library(echarts4r)
+
+simple_funnel_diagram <- function(data, title, theme) {
+  p <- data %>%
+    e_charts() %>%
+    e_funnel(value, key) %>%
+    e_title(title) %>%
+    e_theme(theme)
+  return(p)
+}
+
+# ====================== Plugin Caller ======================
+
+p <- simple_funnel_diagram(
+  data = if (exists("data") && is.data.frame(data)) data else "",
+  title = conf$extra$title,
+  theme = conf$extra$theme
+)
+
+export_htmlwidget(p)
