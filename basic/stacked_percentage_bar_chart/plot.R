f55cbb99cde15e3955d9b9609ab1fff8bf2ce4a0
lxs <liang09255@gmail.com>
2023-07-20 18:28:25 +0800

    feat: add new drawing plugin - Stacked Percentage Bar Chart

---
+++ basic/stacked_percentage_bar_chart/plot.R
@@ -0,0 +1,40 @@
+library(ggplot2)
+library(dplyr)
+
+percentsge_stacked_bar_chart <- function(data, y, title, x_label, y_label, add_percent_label) {
+  data$total <- rowSums(data[, -1])
+  data_long <- tidyr::gather(data, kinds, value, -y, -total)
+  data_long <- data_long %>%
+    group_by({ { y } }) %>%
+    mutate(percent = value / total * 100)
+  data_long[[y]] <- factor(data_long[[y]], levels = data[[y]])
+
+  p <- ggplot(data_long, aes_string(x = "percent", y = y, fill = "kinds")) +
+    geom_bar(stat = "identity", position = "stack") +
+    labs(title = title, x = x_label, y = y_label) +
+    scale_x_continuous(labels = scales::percent_format(scale = 1)) +
+    theme_minimal()
+
+  if (add_percent_label) {
+    p <- p + geom_text(aes(label = ifelse(percent != 0, paste0(round(percent), "%"), "")),
+                       position = position_stack(vjust = 0.5)
+    )
+  }
+
+  return(p)
+}
+
+# ====================== Plugin Caller ======================
+
+p <- percentsge_stacked_bar_chart(
+  data = if (exists("data") && is.data.frame(data)) data else "",
+  y = conf$dataArg[[1]][[1]]$value,
+  title = conf$extra$title,
+  x_label = conf$extra$x_label,
+  y_label = conf$extra$y_label,
+  add_percent_label = conf$extra$add_percent_label
+)
+
+p <- set_complex_general_theme(set_palette_theme(p, conf))
+
+export_single(p)
