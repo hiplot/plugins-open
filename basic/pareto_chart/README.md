4282fa389451ad9a81e07aa3b14f69c317ad6143
lxs <liang09255@gmail.com>
2023-07-21 12:27:30 +0800

    feat: add new drawing plugin - Pareto Chart (#9)

---
+++ basic/pareto_chart/README.md
@@ -0,0 +1,7 @@
+# Pareto Chart
+
+* Case Data Analysis
+
+  The case data represents the sales data of a product on multiple platforms.
+
+  The plugin will automatically plot a bar chart with sales data in descending order and simultaneously calculate the cumulative sales to draw the cumulative line chart.
---
+++ basic/pareto_chart/plot.R
@@ -0,0 +1,31 @@
+library(ggplot2)
+
+pareto_chart <- function(data, x, y) {
+  data <- data[order(-data[[y]]), ]
+  data[[x]] <- factor(data[[x]], levels = data[[x]])
+
+  # calculate percentage number
+  data$accumulating <- cumsum(data[[y]])
+  cal_num <- sum(data[[y]]) / max(data[[y]])
+  data$accumulating <- data$accumulating / cal_num
+
+  p <- ggplot(data, aes_string(x = x, y = y, fill = x)) +
+    geom_bar(stat = "identity") +
+    geom_line(aes(y = accumulating), group = 1) +
+    geom_point(aes(y = accumulating), show.legend = FALSE) +
+    scale_y_continuous(sec.axis = sec_axis(trans = ~ . / max(data$sales) * 100, name = "Percentage"))
+
+  return(p)
+}
+
+# ====================== Plugin Caller ======================
+
+p <- pareto_chart(
+  data = if (exists("data") && is.data.frame(data)) data else "",
+  x = conf$dataArg[[1]][[1]]$value,
+  y = conf$dataArg[[1]][[2]]$value
+)
+
+p <- set_complex_general_theme(set_palette_theme(p, conf))
+
+export_single(p)
